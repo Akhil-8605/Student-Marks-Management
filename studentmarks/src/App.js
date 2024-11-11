bf368ffd4e087a5679639153264b95f89c99e143
Akhil <akhileshadam186@gmail.com>
2024-11-11 16:13:20 +0530

    first commit

---
+++ studentmarks/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SchoolResultChecker from './SchoolResultChecker';
+
+function App() {
+  return (
+   <>
+   <SchoolResultChecker/>
+   </>
+  );
+}
+
+export default App;
